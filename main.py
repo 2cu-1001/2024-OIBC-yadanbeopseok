204600811824369ec78f5b68a92786d339c4e570
2cu-1001 <137245969+2cu-1001@users.noreply.github.com>
2024-11-14 05:15:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,38 @@ from train import OurTrainer
 from make_json2pred import DataCvt
 
 def main():
+    print("#############################################################################")
     data_requester = DataRequester()
-    data_requester.API_KEY = "z"
+    data_requester.API_KEY = ""
     
-    weather_forecast = data_requester.request_actual_weather(date="2024-11-14")
-    weather_real =data_requester.request_weather_forecast(date="2024-11-15")
+    cur_day = "2024-11-12"
+    pre_day = "2024-11-13"
+    nxt = "20241115"
     
-    generation_yesterday = data_requester.request_elec_supply(date="2024-11-14")
+    weather_cur =data_requester.request_actual_weather(date=cur_day)
+    weather_pre = data_requester.request_actual_weather(date=pre_day)
     
-    dataCvt = DataCvt(weather_forecast, weather_real, generation_yesterday)
+    generation_cur = data_requester.request_elec_supply(date=cur_day)
+    generation_pre = data_requester.request_elec_supply(date=pre_day)
+    
+    dataCvt = DataCvt(weather_cur, weather_pre, generation_cur, generation_pre)
+    trainer = OurTrainer()
+    trainer.c2g.max_epoch = 1
+    trainer.g2p.max_epoch = 1
     
     weather_prediction = dataCvt.cvt_weather_data()
+    
+    trainer.train_c2g()
+    
+    predicted_generation = trainer.predict_c2g(weather_prediction)
+    print(predicted_generation)
+    
     supplyNdemand = dataCvt.cvt_generation_data()
+    trainer.train_g2p()
     
-    OurTrainer.train_c2g()
-    OurTrainer.train_g2p()
+    genSupplyDemand_prediction = dataCvt.cvt_generationSupplyDmand_data(predicted_generation, supplyNdemand, nxt)
     
-    predicted_generation = OurTrainer.predict_c2g(weather_prediction)
-    
-    genSupplyDemand_prediction = dataCvt.cvt_generationSupplyDmand_data(predicted_generation, supplyNdemand)
-    
-    predicted_price = OurTrainer.predict_g2p(genSupplyDemand_prediction)
-    
+    predicted_price = trainer.predict_g2p(genSupplyDemand_prediction)
     print(predicted_price)
 
 if __name__ == "__main__":
